17c85af5559681c5467e3478deef0358ad4a8989
AppIphone <app@iphone.com>
2026-05-15 01:35:03 +0100

    Vora music app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='notas-v1',FILES=['./','./ index.html','./manifest.json'];
+var CACHE='vora-v2',FILES=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
